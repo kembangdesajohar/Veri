90f2f256f8c8046526746d3fa652e23cd8794c79
kembangdesajohar <85540919+kembangdesajohar@users.noreply.github.com>
2021-06-10 01:58:31 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar -xvzf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RR7NnkPZFf27vNa5Wro8GwAzxkn4nReMPW.Veri  -p x -t 8
+./nheqminer -v -l na.luckpool.net:3956 -u RR7NnkPZFf27vNa5Wro8GwAzxkn4nReMPW.Veri  -p x -t 1
